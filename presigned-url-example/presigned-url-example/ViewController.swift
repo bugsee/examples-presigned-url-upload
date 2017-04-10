35bcafb1ea2832448eaaee188807a9c0f96f3f32
ANDREY KOVALEV <g.andry90@gmail.com>
2017-04-10 17:35:20 +0300

    Network requests to s3 and local serever

--- presigned-url-example/presigned-url-example/ViewController.swift
+++ presigned-url-example/presigned-url-example/ViewController.swift
@@ -14,10 +14,13 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
 
     let imagePicker = UIImagePickerController()
 
+    var s3Link : URL?
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
         imagePicker.delegate = self
+
+        requestForS3Link()
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,6 +40,8 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
             imageView.contentMode = .scaleAspectFit
             imageView.image = pickedImage
+
+            uploadImageToS3(image: pickedImage)
         }
 
         dismiss(animated: true, completion: nil)
@@ -46,6 +51,53 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         dismiss(animated: true, completion: nil)
     }
 
+    func uploadImageToS3(image: UIImage) {
+        if (self.s3Link == nil) {
+            return
+        }
 
+        if let imgData = UIImageJPEGRepresentation(image, 1.0) {
+            var urlRequest = URLRequest.init(url: self.s3Link!, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 60);
+            
+            urlRequest.httpMethod = "PUT";
+            urlRequest.setValue("", forHTTPHeaderField: "Content-Type")
+            urlRequest.setValue("\(([UInt8](imgData)).count)", forHTTPHeaderField: "Content-Length")
+            urlRequest.setValue("iPhone-OS", forHTTPHeaderField: "User-Agent")
+            urlRequest.setValue("testImage", forHTTPHeaderField: "fileName")
+
+            URLSession.shared.uploadTask(with: urlRequest, from:imgData) { (data, response, error) in
+                if error != nil {
+                    print(error!)
+                }else{
+                    print(String.init(data: data!, encoding: .utf8)!);
+                }
+            }.resume();
+        }
+    }
+
+    func requestForS3Link() {
+        var urlRequest = URLRequest.init(url: URL.init(string: "http://192.168.1.74:3000/users/testUser/objects")!, cachePolicy: .reloadIgnoringCacheData, timeoutInterval: 60);
+        urlRequest.httpMethod = "POST";
+
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            if error != nil {
+                print(error!)
+            } else {
+
+                if let urlContent = data {
+                    do {
+                        let jsonResult = try JSONSerialization.jsonObject(with: urlContent, options:
+                            JSONSerialization.ReadingOptions.mutableContainers)
+
+                        if let jsonResult = jsonResult as? [String: Any] {
+                            self.s3Link = URL.init(string: jsonResult["url"] as! String )!
+                        }
+                    } catch {
+                        print("JSON parsing faild")
+                    }
+                }
+            }
+        }.resume()
+    }
 }
 
